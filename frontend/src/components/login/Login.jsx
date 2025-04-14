cc97ca03415a8a9926a5b494de90a810053fc0a8
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2025-04-14 00:04:16 -0300

    U-tailwind
    Desinstale tailwind por conflictos

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import '../login/login.css'
 import logo from '../../img/logo.png';
+import './login.css'
 const Login = () => {
     return (
         <Form>
@@ -29,10 +29,35 @@ const Login = () => {
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Recuerdame" />
             </Form.Group>
-            <Button className='btn' variant="primary" type="submit">
+            <Button className='b' variant="primary" type="submit">
                 Iniciar Sesión
             </Button>
+
+            <div className="mt-6">
+                <div className="relative">
+                    <div className="absolute inset-0 flex items-center">
+                        <div className="w-full border-t border-gray-300"></div>
+                    </div>
+                    <div className="relative flex justify-center text-sm">
+                        <span className="px-2 bg-white text-gray-500">O Inicia Sesión con</span>
+                    </div>
+                </div>
+            </div>
+
+            <button
+                className="w-full flex items-center justify-center gap-x-3 py-2.5 border rounded-lg text-sm font-medium hover:bg-gray-50 duration-150 active:bg-gray-100"
+            >
+                <img
+                    src="https://raw.githubusercontent.com/sidiDev/remote-assets/7cd06bf1d8859c578c2efbfda2c68bd6bedc66d8/google-icon.svg"
+                    alt="Google"
+                    className="w-5 h-5"
+                />
+
+                 Continuar con Google
+            </button>
         </Form>
+
+
     )
 }
 
