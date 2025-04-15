09e16365fea4e2fed15ca1d3b180954dd3ceac84
Melina Bueno <melinabueno123@gmail.com>
2025-04-15 14:26:07 -0300

    Cambio en ruta register

--- frontend/src/layout/Layout/Layout.jsx
+++ frontend/src/layout/Layout/Layout.jsx
@@ -1,26 +1,20 @@
-import React, { useState } from 'react';
-import { Outlet, useLocation } from 'react-router-dom'; // Importamos useLocation
-import NavBar from '../../components/NavBar/NavBar';
-import Login from '../../components/Login/Login';
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "../../components/NavBar/NavBar";
+import Login from "../../components/Login/Login";
 
 const Layout = () => {
+  
   const [showLogin, setShowLogin] = useState(false);
-  const location = useLocation(); // Obtiene la ubicación actual
-
-  // Verificamos si estamos en la ruta '/register' y si es así, no mostramos el NavBar
-  const hideNav = location.pathname === '/register';
 
   return (
     <>
-      {/* Solo mostramos el NavBar si la ruta no es '/register' */}
-      {!hideNav && <NavBar showLogin={showLogin} setShowLogin={setShowLogin} />}
-      
-      {/* Renderiza el contenido de la ruta */}
+      {<NavBar showLogin={showLogin} setShowLogin={setShowLogin} />}
+
       <Outlet />
 
       {/*<Footer />*/}
 
-      {/* Mostrar el Login en cualquier ruta */}
       <Login showLogin={showLogin} />
     </>
   );
