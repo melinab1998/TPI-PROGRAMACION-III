d13bd5dda4a8a7f785b6ec197699236d768063dc
lautiasc10 <lautiasc10@gmail.com>
2025-04-14 23:10:22 -0300

    Creamos la pagina Registro y realizamos algunos cambios.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout/Layout"
 import Home from "./pages/Home/Home"
+import Register from "./pages/Register/Register";
+
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home/>} />
+        <Route path="register" element={<Register />} />
       </Route>
     </Routes>
   );
