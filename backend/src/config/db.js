5d11e195358aa148e67aab9626ab1ed6bbf830db
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2025-04-19 09:25:02 -0300

    config-coments

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: 'postgres',
     port: process.env.DB_PORT,
-    logging: false,
+    logging: false,// Si ponés true, Sequelize te muestra las consultas SQL en consola
   }
 );
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware para parsear JSON
+// no olvidar de instalar morgan
 app.use(express.json());
 
 app.get('/', (req, res) => {
