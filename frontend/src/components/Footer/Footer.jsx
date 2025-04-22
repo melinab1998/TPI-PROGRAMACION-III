bd504f3cc1c89b7a90a2c6cf5b89df8e6cf320e0
Ivan-1889 <ivanberrino10@hotmail.com>
2025-04-22 18:34:30 -0300

    footer-bootstrap

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,50 +1,68 @@
-import React from 'react';
-import './Footer.css';
-import logo from '../../img/logo.png';
-import facebookIcon from '../../img/facebook-icon.svg';
-import instagramIcon from '../../img/instagram-icon.svg';
-import whatsappIcon from '../../img/whatsapp-icon.svg';
+import { Container, Row, Col } from "react-bootstrap";
+import "./Footer.css";
+import footerLogo from "../../img/logo.png";
+import facebookIcon from "../../img/facebook-icon.svg";
+import instagramIcon from "../../img/instagram-icon.svg";
+import whatsappIcon from "../../img/whatsapp-icon.svg";
 
 const Footer = () => {
     return (
-        <footer className="bg-light py-5 mt-5">
-            <div className="container">
-            <div className="row text-center text-md-start align-items-center"> 
-    <div className="col-md-4 text-center mb-4 mb-md-0">
-        <h5 className="text-danger">Información</h5>
-        <ul className="list-unstyled">
-            <li><a href="#about-us" className="links-footer">Sobre nosotros</a></li>
-            <li><a href="#" className="links-footer">Medios de contacto</a></li>
-            <li><a href="#" className="links-footer">Aplicación móvil</a></li>
-            <li><a href="#" className="links-footer">¿Cómo ayudar?</a></li>
-        </ul>
-    </div>
+    <footer className="bg-light py-5 mt-5 border-top">
+        <Container>
+        <Row className="text-center text-md-start">
+            <Col md={4} className="d-flex flex-column align-items-center justify-content-center text-center">
+            <h5 className="text-danger">Información</h5>
+            <ul className="list-unstyled">
+                <li>
+                <a href="#sobrenosotros">Sobre nosotros</a>
+                </li>
+                <li>
+                <a href="#">Medios de contacto</a>
+                </li>
+                <li>
+                <a href="#">Aplicación móvil</a>
+                </li>
+                <li>
+                <a href="#">¿Cómo ayudar?</a>
+                </li>
+            </ul>
+            </Col>
 
-
-                    <div className="col-md-4 text-center mb-4 mb-md-0">
-                        <img src={logo} alt="Mi Hogar Logo" style={{ maxWidth: '80px' }} />
-                        <div className="mt-3">
-                            <a href="#"><img src={facebookIcon} alt="Facebook" className="mx-2" style={{ width: '24px' }} /></a>
-                            <a href="#"><img src={instagramIcon} alt="Instagram" className="mx-2" style={{ width: '24px' }} /></a>
-                            <a href="#"><img src={whatsappIcon} alt="WhatsApp" className="mx-2" style={{ width: '24px' }} /></a>
-                        </div>
-                    </div>
-
-                    <div className="col-md-4 text-center mb-4 mb-md-0">
-                        <h5 className="text-danger">Términos y configuración</h5>
-                        <ul className="list-unstyled">
-                            <li><a href="#" className="links-footer">Privacidad y cookies</a></li>
-                            <li><a href="#" className="links-footer">Términos y condiciones</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <hr />
-                <div className="text-center mt-3">
-                    <p className="mb-0">Copyright © 2025 - Mi Hogar. Todos los derechos reservados</p>
-                </div>
+            <Col md={4} className="d-flex flex-column align-items-center justify-content-center text-center my-4 my-md-0">
+            <img src={footerLogo} alt="Mi Hogar Logo" style={{ width: "80px" }} />
+            <div className="mt-3 d-flex justify-content-center gap-3">
+                <a href="#">
+                <img src={facebookIcon} alt="Facebook" width="24" />
+                </a>
+                <a href="#">
+                <img src={instagramIcon} alt="Instagram" width="24" />
+                </a>
+                <a href="#">
+                <img src={whatsappIcon} alt="WhatsApp" width="24" />
+                </a>
             </div>
-        </footer>
+            </Col>
+
+            <Col md={4} className="d-flex flex-column align-items-center justify-content-center text-center my-10">
+            <h5 className="text-danger">Términos y configuración</h5>
+            <ul className="list-unstyled">
+            <li>
+                <a href="#">Privacidad y cookies</a>
+            </li>
+            <li>
+                <a href="#">Términos y condiciones</a>
+            </li>
+            </ul>
+            </Col>
+        </Row>
+
+        <hr className="my-4" />
+
+        <div className="text-center text-muted small">
+            © 2025 - Mi Hogar. Todos los derechos reservados.
+        </div>
+        </Container>
+    </footer>
     );
 };
 
