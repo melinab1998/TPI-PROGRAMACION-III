4e874419eb1f08c4a83e7eb775b3f66ef0a6eb6f
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2025-04-22 20:33:48 -0300

    testing-useState-form

--- frontend/src/pages/AdoptionForm/AdoptionForm.jsx
+++ frontend/src/pages/AdoptionForm/AdoptionForm.jsx
@@ -1,28 +1,192 @@
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import React from 'react'
+import { useState } from 'react';
 
 const AdoptionForm = () => {
+  const [hasPets, setHasPets] = useState(null);
+  const [hasPermission, setHasPermission] = useState(null);
+
+  const handlePetStatus = (e) => {
+    setHasPets(e.target.value);
+    console.log(hasPets);
+
+  }
   return (
     <Form>
-{/*     <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-      <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-      </Form.Text>
-    </Form.Group>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridName">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control type="text" placeholder="Nombre" />
+        </Form.Group>
 
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button> */}
-  </Form>
+        <Form.Group as={Col} controlId="formGridLastname">
+          <Form.Label>Apellido</Form.Label>
+          <Form.Control type="text" placeholder="Apellido" />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Dirección</Form.Label>
+        <Form.Control placeholder="San Lorenzo 2500" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Dirección 2(Opcional)</Form.Label>
+        <Form.Control placeholder="Apartamento" />
+      </Form.Group>
+
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>Localidad</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        {/*         <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Select defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Select>
+        </Form.Group> */}
+
+        <Form.Group as={Col} controlId='formGridDni'>
+          <Form.Label>DNI</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+
+      {/*       <Form.Group className="mb-3" id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+ */}
+      {/*       <Button variant="primary" type="submit">
+        Submit
+      </Button> */}
+
+      <Form.Group>
+        <Form.Label>¿En qué tipo de vivienda habitás?</Form.Label>
+        <div className="mb-3">
+          <Form.Check
+            type="radio"
+            label="CASA"
+            name="housingType"
+            id="housing-house"
+          />
+          <Form.Check
+            type="radio"
+            label="DEPARTAMENTO"
+            name="housingType"
+            id="housing-apartment"
+          />
+        </div>
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Seleccioná la opción correcta:</Form.Label>
+        <div className="mb-3">
+          <Form.Check
+            type="radio"
+            label="PROPIETARIO"
+            name="ownershipStatus"
+            id="ownership-owner"
+          />
+          <Form.Check
+            type="radio"
+            label="INQUILINO"
+            name="ownershipStatus"
+            id="ownership-tenant"
+          />
+        </div>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>¿Cuenta con patio la vivienda?</Form.Label>
+        <div className="mb-3">
+          <Form.Check
+            type="radio"
+            label="Sí"
+            name="courtyardStatus"
+            id="courtyard-ownership"
+          />
+          <Form.Check
+            type="radio"
+            label="No"
+            name="courtyardStatus"
+            id="courtyard-ownership"
+          />
+        </div>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>En caso de alquilar, ¿lo consultaste con el dueño?</Form.Label>
+        <div className="mb-3">
+          <Form.Check
+            type="radio"
+            label="Sí, pregunté y admite mascotas"
+            name="ownerConsultation"
+            id="owner-yes"
+          />
+          <Form.Check
+            type="radio"
+            label="No pregunté, pero el edificio admite"
+            name="ownerConsultation"
+            id="owner-building-allows"
+          />
+          <Form.Check
+            type="radio"
+            label="No pregunté"
+            name="ownerConsultation"
+            id="owner-no"
+          />
+        </div>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>¿Tenés otros animales?</Form.Label>
+        <div className="mb-3">
+          <Form.Check
+            type="radio"
+            label="Sí"
+            name="petStatus"
+            id="pets-yes"
+            value="yes"
+            onChange={handlePetStatus}
+          />
+          <Form.Check
+            type="radio"
+            label="No"
+            name="petStatus"
+            id="pets-no"
+            value="no"
+            onChange={handlePetStatus}
+          />
+        </div>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>En caso afirmativo, ¿están castrados?</Form.Label>
+        <div className="mb-3">
+          <Form.Check
+            type="radio"
+            label="Sí"
+            name="petsNeutered"
+            id="pets-neutered-yes"
+            disabled={hasPets !== 'yes'}
+          />
+          <Form.Check
+            type="radio"
+            label="No"
+            name="petsNeutered"
+            id="pets-neutered-no"
+            disabled={hasPets !== 'yes'}
+          />
+          <Form.Check
+            type="radio"
+            label="Algunos sí, otros no"
+            name="petsNeutered"
+            id="pets-neutered-some"
+            disabled={hasPets !== 'yes'}
+          />
+        </div>
+      </Form.Group>
+
+    </Form>
   )
 }
 
