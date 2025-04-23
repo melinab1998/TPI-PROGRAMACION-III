2b79c249a06af5001e1e2ea6eef1551b997b58e3
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2025-04-23 19:03:20 -0300

    db-config

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,44 +1,34 @@
-import express from 'express';
-import sequelize from './config/db.js';
-import User from './models/User.js'; 
-
+import express from "express";
+import sequelize from "./config/db.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT || 3000;
 
 // Middleware para parsear JSON
 // no olvidar de instalar morgan
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Backend funcionando!');
+app.get("/", (req, res) => {
+	res.send("Backend funcionando!");
 });
 
-/* app.post('/users', async (req, res) => {
-  try {
-    const { name, lastName, email, age, mobile, password } = req.body;
-    const newUser = await User.create({ name, lastName, email, age, mobile, password });
-    res.status(201).json(newUser);
-  } catch (err) {
-    res.status(500).json({ message: 'Error creando usuario', error: err.message });
-  }
-}); */
-sequelize.authenticate()
+app.listen(PORT, () => {
+	console.log(`servidor corriendo en http://localhost:${PORT}`);
+});
+
+sequelize
+  .authenticate()
   .then(() => {
-    console.log('✅ Conectado a la base de datos');
-    
-    // Sincronizar los modelos
-    sequelize.sync({ force: false })
+    console.log("Conectado a la base de datos");
+    sequelize
+      .sync({ force: false })
       .then(() => {
-        console.log('✅ Sincronización exitosa');
-        app.listen(PORT, () => {
-          console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-        });
+        console.log("Sincronización exitosa");
       })
-      .catch(err => {
-        console.error('❌ Error al sincronizar los modelos:', err);
+      .catch((err) => {
+        console.error("Error al sincronizar la base de datos", err);
       });
   })
-  .catch(err => {
-    console.error('❌ Error al conectar con la base de datos:', err);
+  .catch((err) => {
+    console.error("Error al conectar a la base de datos", err);
   });
