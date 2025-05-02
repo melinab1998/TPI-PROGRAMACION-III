d4f9f6103d7b12a05c2fd426afd34749c955dad8
Melina Bueno <melinabueno123@gmail.com>
2025-05-01 21:42:43 -0300

    btn-change

--- frontend/src/components/AdminDashboard/AdminDashboard.jsx
+++ frontend/src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "./AdminDashboard.css"; // Asegúrate de importar el archivo CSS
+import "./AdminDashboard.css"; 
 
 const AdminDashboard = () => {
   return (
