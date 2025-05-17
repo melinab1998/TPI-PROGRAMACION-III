ea5b936e80315dd147e02692505ac62e7ae8919e
Melina Bueno <melinabueno123@gmail.com>
2025-05-17 16:17:19 -0300

    verify-token

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,13 +1,14 @@
 import { createUser, loginUser, getUsers, updateUserRole, deleteUser } from "../services/users.services.js";
 import {Router} from "express";
+import { verifyToken } from "../middlewares/auth.js";
 
 const router = Router();
 
-router.get('/api/users', getUsers);  
+router.get('/api/users', verifyToken, getUsers);  
 router.post("/api/register", createUser);
 router.post("/api/login", loginUser);
-router.put('/api/users/:id/role', updateUserRole);
-router.delete('/api/users/:id', deleteUser);
+router.put('/api/users/:id/role', verifyToken, updateUserRole);
+router.delete('/api/users/:id', verifyToken, deleteUser);
 
 export default router;
 
