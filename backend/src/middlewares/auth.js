517c797702d190a684a68f997808a69682645857
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2025-05-18 01:29:51 -0300

    Update auth.js

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,28 +1,26 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 dotenv.config();
 
-export const verifyToken = (req, res, next) =>{
-    const header = req.header("Authorization");
+export const verifyToken = (req, res, next) => {
+	const header = req.header("Authorization");
 
-    if(!header){
-        return res.status(401).json({message: 'No tiene autorizacion requerida'});
-    }
+	if (!header) {
+		return res.status(401).json({ message: "No tiene autorizacion requerida" });
+	}
 
-    const token = header.split(" ")[1];
+	const token = header.split(" ")[1];
 
-    if(!token){
-        return res.status(401).json({message: 'No tiene autorizacion requerida'});
-    }
-    
-   try{
-        const payload = jwt.verify(token, process.env.SECRETKEY);
+	if (!token) {
+		return res.status(401).json({ message: "No tiene autorizacion requerida" });
+	}
 
-        console.log(payload)
+	try {
+		const payload = jwt.verify(token, process.env.SECRETKEY);
+		console.log(payload);
 
-        next();
-
-   } catch(error){
-    return res.status(403).json({message: 'No posee permisos correctos'});
-   }
-}
+		next();
+	} catch (error) {
+		return res.status(403).json({ message: "No posee permisos correctos" });
+	}
+};
