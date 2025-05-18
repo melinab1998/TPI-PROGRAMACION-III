6613c706b3c872fd157b217b7d02b6f711eb067f
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2025-05-17 21:38:37 -0300

    Update requests.routes.js

--- backend/src/routes/requests.routes.js
+++ backend/src/routes/requests.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createRequest } from "../services/request.services.js";
 import { verifyToken } from "../middlewares/auth.js";
+import { getPetById } from "../services/pets.services.js";
 
 const router = Router();
 
 router.post("/api/adoption/:id", verifyToken, createRequest);
+router.get("/api/adoption/:id", getPetById);
 
 export default router;
