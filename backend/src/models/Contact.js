e6559ac898bdfb63cd0682e9cd50ab0d31aaabd3
lautiasc10 <lautiasc10@gmail.com>
2025-05-21 13:04:04 -0300

    Creacion de la tabla Contact

---
+++ backend/src/models/Contact.js
@@ -0,0 +1,31 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+export const Contact = sequelize.define( 
+	"Contact",
+	{
+		id_contact: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		message: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		}
+	},
+	{
+		tableName: "contacts",
+		timestamps: false,
+	}
+);
+export default Contact;
