36b1c1403af1b37460280551868d2fab8e772496
Ivan-1889 <ivanberrino10@hotmail.com>
2025-05-23 21:27:48 -0300

    add functionality to the Facebook icon

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -166,7 +166,7 @@ const Register = () => {
           }}>Inicia Sesión</a>
         </p>
         <div className="networks mt-3">
-          <a href=""><FaFacebook size={40} /></a>
+          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><FaFacebook size={40} /></a>
           <a href="https://x.com/" target="_blank" rel="noopener noreferrer"><BsTwitterX size={40} color="black" /></a>
           <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><FaInstagram size={40} color="#E1306C" /></a>
         </div>
