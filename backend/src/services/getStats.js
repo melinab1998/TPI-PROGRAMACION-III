e0b46251afe4b6f85ed947a3f6a872acc8c9e9d6
lautiasc10 <lautiasc10@gmail.com>
2025-05-25 12:40:20 -0300

    services de datos generales de la pagina

--- backend/src/services/getStats.js
+++ backend/src/services/getStats.js
@@ -1,10 +1,14 @@
 import Pet from "../models/Pet.js";
 import Request  from "../models/Request.js";
 
-export const getStats = async(req, res) =>{
-    try{
-        const petsInAdoption = await Pet.count({where:{adopted: false}});
-        const pendingRequests = await Request.count({where:{state: 'pendiente'}});
-        const successfulAdoptions = await Request.count({where:{state: 'Aprobada'}});
-    }
-}
+export const getStats = async() =>{
+    const petsInAdoption = await Pet.count({where:{adopted: false}});
+    const pendingRequests = await Request.count({where:{state: 'pendiente'}});
+    const successfulAdoptions = await Request.count({where:{state: 'Aprobada'}});
+
+    return{
+        petsInAdoption,
+        pendingRequests,
+        successfulAdoptions
+    };
+};
