dcde6f6db6bf52ee454fd60539caf1932688e16f
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2025-05-31 22:03:51 -0300

    post-password-route-test

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,14 +1,31 @@
-import { createUser, loginUser, getUsers, updateUserRole, deleteUser } from "../services/users.services.js";
-import {Router} from "express";
+import {
+	createUser,
+	loginUser,
+	getUsers,
+	updateUserRole,
+	deleteUser,
+	forgotPassword,
+} from "../services/users.services.js";
+import { Router } from "express";
 import { verifyToken, authorizeRoles } from "../middlewares/auth.js";
 
 const router = Router();
 
-router.get('/api/users', verifyToken, authorizeRoles("superadmin"), getUsers);  
+router.get("/api/users", verifyToken, authorizeRoles("superadmin"), getUsers);
+router.post("/api/forgot-password", forgotPassword);
 router.post("/api/register", createUser);
 router.post("/api/login", loginUser);
-router.put('/api/users/:id/role', verifyToken, authorizeRoles("superadmin"), updateUserRole);
-router.delete('/api/users/:id', verifyToken, authorizeRoles("superadmin"), deleteUser);
+router.put(
+	"/api/users/:id/role",
+	verifyToken,
+	authorizeRoles("superadmin"),
+	updateUserRole
+);
+router.delete(
+	"/api/users/:id",
+	verifyToken,
+	authorizeRoles("superadmin"),
+	deleteUser
+);
 
 export default router;
-
